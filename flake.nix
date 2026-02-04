56349d694e36aa19d0a489946890dbbb3157b1da
luna <luna@wvvw.me>
2026-02-04 21:02:29 +0000

    fix: upgrade to nixos-unstable for newer go versions

--- flake.nix
+++ flake.nix
@@ -1,15 +1,13 @@
 {
   description = "Luna's NixOS configurations";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";  # Changed from 24.05
     
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, sops-nix, ... }: {
     nixosConfigurations = {
       nova = nixpkgs.lib.nixosSystem {
