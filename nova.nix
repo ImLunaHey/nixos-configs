c007c3aaa53bf51c54c160ec3a41ce42b20a4fce
luna <luna@wvvw.me>
2026-02-04 21:05:53 +0000

    fix(nova): rename vaapiVdpau to libva-vdpau-driver

--- nova.nix
+++ nova.nix
@@ -39,7 +39,7 @@
     extraPackages = with pkgs; [
       intel-media-driver
       intel-vaapi-driver
-      vaapiVdpau
+      libva-vdpau-driver  # Changed from vaapiVdpau
       libvdpau-va-gl
     ];
   };
