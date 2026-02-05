a9906e20637c248046d68d2b9681a8ddc0276306
luna <luna@wvvw.me>
2026-02-05 12:29:33 +0000

    fix(gilbert): add setupSecrets dependency to ARM config script
    Ensure secrets are available before substitution runs

--- machines/gilbert/default.nix
+++ machines/gilbert/default.nix
@@ -76,14 +76,17 @@
   ];
 
   # Generate ARM config with secrets substituted
-  system.activationScripts.arm-config = ''
-    ${pkgs.gnused}/bin/sed \
-      -e "s|@MAKEMKV_KEY@|$(cat ${config.sops.secrets.makemkv_key.path})|g" \
-      -e "s|@OMDB_API_KEY@|$(cat ${config.sops.secrets.omdb_api_key.path})|g" \
-      ${./arm-config/arm.yaml} > /mnt/media/config/arm.yaml
-    chown 1000:1000 /mnt/media/config/arm.yaml
-    chmod 644 /mnt/media/config/arm.yaml
-  '';
+  system.activationScripts.arm-config = {
+    deps = [ "setupSecrets" ];
+    text = ''
+      ${pkgs.gnused}/bin/sed \
+        -e "s|@MAKEMKV_KEY@|$(cat ${config.sops.secrets.makemkv_key.path})|g" \
+        -e "s|@OMDB_API_KEY@|$(cat ${config.sops.secrets.omdb_api_key.path})|g" \
+        ${./arm-config/arm.yaml} > /mnt/media/config/arm.yaml
+      chown 1000:1000 /mnt/media/config/arm.yaml
+      chmod 644 /mnt/media/config/arm.yaml
+    '';
+  };
 
   # Automatic Ripping Machine container
   virtualisation.oci-containers = {
