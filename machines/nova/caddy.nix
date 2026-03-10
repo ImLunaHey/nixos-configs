149f21e78536d7d321780c125f253ff63634f47a
luna <luna@wvvw.me>
2026-03-10 16:42:42 +0000

    fix(nova): update caddy cloudflare plugin hash

--- machines/nova/caddy.nix
+++ machines/nova/caddy.nix
@@ -6,7 +6,7 @@
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.3" ];
-      hash = "sha256-bJO2RIa6hYsoVl3y2L86EM34Dfkm2tlcEsXn2+COgzo=";
+      hash = "sha256-bL1cpMvDogD/pdVxGA8CAMEXazWpFDBiGBxG83SmXLA=";
     };
     virtualHosts."matrix.flaked.org" = {
       extraConfig = ''
