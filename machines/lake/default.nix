6cc4b9c2144112ec6a90e1c4780d714ed1addaa1
luna <luna@wvvw.me>
2026-09-08 18:05:55 +0100

    feat(lake): enable ZFS foundation

--- machines/lake/default.nix
+++ machines/lake/default.nix
@@ -11,6 +11,13 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "zfs" ];
+  boot.zfs.forceImportRoot = false;
+
+  services.zfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+  };
 
   system.stateVersion = "26.05";
 
