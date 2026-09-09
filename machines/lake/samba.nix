bba665f73bf10fd9544de314b4910a2c63556b9d
luna <luna@wvvw.me>
2026-09-09 09:42:13 +0100

    feat(lake): add Time Machine share

--- machines/lake/samba.nix
+++ machines/lake/samba.nix
@@ -19,6 +19,26 @@ in
     "rips"
   ];
 
+  systemd.services.lake-timemachine-dataset = {
+    description = "Create the Lake Time Machine dataset";
+    after = [ "zfs-import-storage.service" ];
+    before = [ "samba-smbd.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = ''
+      if ! ${pkgs.zfs}/bin/zfs list -H storage/timemachine >/dev/null 2>&1; then
+        ${pkgs.zfs}/bin/zfs create \
+          -o mountpoint=/mnt/storage/timemachine \
+          storage/timemachine
+      fi
+
+      ${pkgs.zfs}/bin/zfs set quota=500G storage/timemachine
+    '';
+  };
+
   systemd.services.samba-setup-users = {
     description = "Configure Samba user accounts";
     after = [ "sops-install-secrets.service" ];
@@ -51,6 +71,21 @@ in
       backups = share "/mnt/storage/backups";
       games = share "/mnt/storage/games";
       rips = share "/mnt/storage/rips";
+      timemachine = (share "/mnt/storage/timemachine") // {
+        "vfs objects" = "catia fruit streams_xattr";
+        "fruit:metadata" = "stream";
+        "fruit:model" = "MacSamba";
+        "fruit:time machine" = "yes";
+        "fruit:time machine max size" = "500G";
+      };
+    };
+  };
+
+  services.avahi = {
+    enable = true;
+    publish = {
+      enable = true;
+      userServices = true;
     };
   };
 
@@ -61,6 +96,6 @@ in
 
   networking.firewall.interfaces.enp100s0f1np1 = {
     allowedTCPPorts = [ 139 445 5357 ];
-    allowedUDPPorts = [ 137 138 3702 ];
+    allowedUDPPorts = [ 137 138 3702 5353 ];
   };
 }
